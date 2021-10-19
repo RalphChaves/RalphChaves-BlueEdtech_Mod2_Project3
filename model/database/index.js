d5a9a92d68f79f3cfc8eb08487594111f43c7d0b
RalphChaves <ralphchaves@gmail.com>
2021-10-19 20:27:41 -0300

    up

--- model/database/index.js
+++ model/database/index.js
@@ -1,18 +1,33 @@
-const {Sequelize} = require("sequelize");
+// const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER, process.env.DB_pass, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres'
-});
+// const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER, process.env.DB_pass, {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'postgres'
+// });
 
-async function conectado(){
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+const Sequelize = require("sequelize");
 
-module.exports = {sequelize, conectado}
+const sequelize = new Sequelize(process.env.DATABASE_URL, {     
+  dialect: 'postgres',
+  protocol: 'postgres',     
+  dialectOptions: {         
+    ssl: {             
+      require: true,             
+      rejectUnauthorized: false         
+    }     
+  } 
+}) 
+
+module.exports = sequelize;
+
+// async function conectado(){
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Connection has been established successfully.');
+//     } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//     }
+// }
+
+// module.exports = {sequelize, conectado}
